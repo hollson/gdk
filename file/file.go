25d395bf2f9af68d7694dd49ea5f0affcf64a459
hollson <hollson@qq.com>
2020-05-26 16:28:44 +0800

    fix memory

---
+++ file/file.go
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------
+// @ Copyright (C) free license,without warranty of any kind .
+// @ Author: hollson <hollson@live.cn>
+// @ Date: 2020-05-26
+// @ Version: 1.0.0
+//
+// Here's the code description...
+// -----------------------------------------------------------------------
+
+package file
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// getFileSize get file size by path(B)
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
+
+// getFileSize get file size by path(B)
+func FileSize(path string) int64 {
+	if !Exists(path) {
+		return 0
+	}
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return 0
+	}
+	return fileInfo.Size()
+}
+
+
+// exists Whether the path exists
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
