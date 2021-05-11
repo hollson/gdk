5615116c680e4b4ee0a2418fd09eb7bb60bfdd2c
hollson <hollson@qq.com>
2021-05-11 15:39:36 +0800

    Rebuilded at 2021年05月11日 15时39分36秒

---
+++ rats/tars_test.go
@@ -0,0 +1,25 @@
+// Copyright 2021 Hollson. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package rats
+
+import (
+    "testing"
+)
+
+func TestFibonacci46(t *testing.T) {
+    Fibonacci(46)
+}
+
+func TestFibonacci47(t *testing.T) {
+    Fibonacci(47)
+}
+
+func TestFibonacci48(t *testing.T) {
+    Fibonacci(48)
+}
+
+func TestFibonacci49(t *testing.T) {
+    Fibonacci(49)
+}
